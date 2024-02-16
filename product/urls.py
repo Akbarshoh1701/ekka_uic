af7c6da0643c7d2f43ae35452ebc828e2ece592e
akbar <akbarsuvonov5157@gmail.com>
2024-02-16 19:00:38 +0500

    feat: detail

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,8 @@
 from product.views import *
 from django.urls import path
+from product.views import ShopListView, ShopDetailView
 
 urlpatterns = [
+    path('ShopListView/', ShopListView.as_view(), name='Product'),
+    path('product/<int:pk>/', ShopDetailView.as_view(), name='product-detail')
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import View
+
+from product.models import Product, Size, Category, Color, ProductImage
+
 
 # Create your views here.
+
+
+class ShopListView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            "product_ls": Product.objects.all(),
+            "size_ls": Size.objects.all(),
+            "categories": Category.objects.all(),
+            "color": Color.objects.all(),
+            "product_image": ProductImage.all()
+        }
+        return render(request, 'product_list.html', context)
+
+class ShopDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        product = Product.objects.get(id=pk)
+        context = {
+            'product': product
+        }
+        return render(request, 'product-details.html', context)
+
